2b99c1e32fe766c6c61f2b3f014e41e057c8e5bb
augustrush <augustrush0923@gmail.com>
2023-11-25 19:39:22 +0800

    路由修改

--- routers/adminRouters.go
+++ routers/adminRouters.go
@@ -9,21 +9,22 @@ import (
 func AdminRouterInit(r *gin.Engine) {
 	adminRouters := r.Group("/admin")
 	{
-		adminRouters.POST("/login", admin.AdminController{}.Login)                        // admin页面登录
-		adminRouters.POST("/logout", admin.AdminController{}.Logout)                      // admin页面登出
-		adminRouters.GET("/news_review", admin.AdminController{}.NewsReview)              // 待审核新闻列表
-		adminRouters.GET("/news_review/detail", admin.AdminController{}.NewsReviewDetail) // 待审核新闻详情
-		adminRouters.PUT("/news_review/audit", admin.AdminController{}.NewsReviewAudit)   // 审核新闻
-		adminRouters.GET("/news_list", admin.AdminController{}.NewsList)                  // 新闻列表
-		adminRouters.GET("/news/:news_id", admin.AdminController{}.NewsReviewDetail)      // 新闻详情
-		adminRouters.PUT("/news/:news_id", admin.AdminController{}.EditNews)              // 新闻编辑
-		adminRouters.GET("/categories", admin.AdminController{}.CategoryList)             // 分类列表
-		adminRouters.PUT("/category/:category_id", admin.AdminController{}.EditCategory)  // 分类编辑
-		adminRouters.POST("/category", admin.AdminController{}.AddCategory)               // 分类新增
-		adminRouters.GET("/user_count", admin.AdminController{}.UserCount)                // 用户统计
+		adminRouters.POST("/login", admin.AdminController{}.Login) // admin页面登录
+
 	}
-	adminUseMiddlewareRouters := r.Group("/admin", middlewares.LoginRequireMiddleware)
+	adminLoginRequireRouters := r.Group("/admin", middlewares.LoginRequireMiddleware)
 	{
-		adminUseMiddlewareRouters.GET("/index", admin.AdminController{}.Index) // admin页面首页
+		adminLoginRequireRouters.GET("/index", admin.AdminController{}.Index)                         // admin页面首页
+		adminLoginRequireRouters.POST("/logout", admin.AdminController{}.Logout)                      // admin页面登出
+		adminLoginRequireRouters.GET("/news_review", admin.AdminController{}.NewsReview)              // 待审核新闻列表
+		adminLoginRequireRouters.GET("/news_review/detail", admin.AdminController{}.NewsReviewDetail) // 待审核新闻详情
+		adminLoginRequireRouters.PUT("/news_review/audit", admin.AdminController{}.NewsReviewAudit)   // 审核新闻
+		adminLoginRequireRouters.GET("/news_list", admin.AdminController{}.NewsList)                  // 新闻列表
+		adminLoginRequireRouters.GET("/news/:news_id", admin.AdminController{}.NewsReviewDetail)      // 新闻详情
+		adminLoginRequireRouters.PUT("/news/:news_id", admin.AdminController{}.EditNews)              // 新闻编辑
+		adminLoginRequireRouters.GET("/categories", admin.AdminController{}.CategoryList)             // 分类列表
+		adminLoginRequireRouters.PUT("/category/:category_id", admin.AdminController{}.EditCategory)  // 分类编辑
+		adminLoginRequireRouters.POST("/category", admin.AdminController{}.AddCategory)               // 分类新增
+		adminLoginRequireRouters.GET("/user_count", admin.AdminController{}.UserCount)                // 用户统计
 	}
 }
